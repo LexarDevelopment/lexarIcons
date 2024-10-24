17a6c960bb56cf820b3cf7a26ab6d0dd156fd513
LexarDevelopment <iokua1@gmail.com>
2024-10-24 15:26:14 -0700

    dist recopy

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 
-gulp.task('copy-and-rename', function() {
-  return gulp.src('style.css')
+gulp.task('copy-all', function() {
+  return gulp.src('src/**/*')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('rename-css', function() {
+  return gulp.src('src/style.css')
     .pipe(rename('lexarIcons.css'))
     .pipe(gulp.dest('dist'));
 });
 
 // Default task
-gulp.task('default', gulp.series('copy-and-rename'));
+gulp.task('default', gulp.series('copy-all', 'rename-css'));
