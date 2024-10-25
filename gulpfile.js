7963a4dd1205ce18392b9805b402666a42a8c8f5
LexarDevelopment <iokua1@gmail.com>
2024-10-25 10:33:45 -0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
+const replace = require('gulp-replace');
 
-gulp.task('copy-all', function() {
-  return gulp.src('src/**/*')
+const baseUrl = 'https://lexarprojects.blob.core.windows.net/lexaricons/';
+
+gulp.task('copy-and-modify-css', function() {
+  return gulp.src('src/style.css')
+    .pipe(replace(/url\('fonts\//g, `url('${baseUrl}fonts/`))
+    // .pipe(rename('lexarIcons.css'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('rename-css', function() {
-  return gulp.src('src/style.css')
-    .pipe(rename('lexarIcons.css'))
+gulp.task('copy-other-files', function() {
+  return gulp.src(['src/**/*', '!src/style.css'])
     .pipe(gulp.dest('dist'));
 });
 
 // Default task
-gulp.task('default', gulp.series('copy-all', 'rename-css'));
+gulp.task('default', gulp.series('copy-and-modify-css', 'copy-other-files'));
